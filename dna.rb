28d22bfd836e743c0cddd1cfe40c6f80c4da0dfd
Persa Zula <persa@persazula.com>
2013-10-02 10:08:35 -0600

    Counting the nucleotides with each_with_object

--- dna.rb
+++ dna.rb
@@ -2,40 +2,53 @@ class DNA
   attr_reader :nucleotides 
 
   def initialize(input)
-    # if validate(input)
-    #   @nucleotides = input
-    # else 
-    #   raise ArgumentError.new("Sorry, U is not a valid nucleotide")
-    # end
-
-    unless validate(input)
-      raise ArgumentError.new("Sorry, #{input} is not a valid nucleotide")
+    if validate_dna(input)
+      @nucleotides = input
+    else 
+      raise ArgumentError.new("Sorry, U is not a valid nucleotide")
     end
-    @nucleotides = input
+
+    # unless validate(input)
+    #   raise ArgumentError.new("Sorry, #{input} is not a valid nucleotide")
+    # end
+    # @nucleotides = input
   end
 
   def dna_nucleotides
     ['A', 'T', 'C', 'G']
   end
 
-  def validate(input)
+  def all_nucleotides
+    dna_nucleotides + ['U']
+  end
+
+  def validate_dna(input)
     letters = input.split("")
     letters.all?{|letter| dna_nucleotides.include?(letter)}
   end
 
+  def validate_dna_or_rna(input)
+    letters = input.split("")
+    letters.all?{|letter| all_nucleotides.include?(letter)}
+  end
+
   def count(type)
-    if type != "X"
+    if validate_dna_or_rna(type)
      nucleotides.count(type)
    else
-    raise ArgumentError.new("Sorry, X is not a valid nucleotide")
+    raise ArgumentError.new("Sorry, #{type} is not a valid nucleotide")
     end
   end
 
   def nucleotide_counts
-    {'A' => count('A'), 
-      'T' => count('T'), 
-      'C' => count('C'), 
-      'G' => count('G')}
+    # {'A' => count('A'), 
+    #   'T' => count('T'), 
+    #   'C' => count('C'), 
+    #   'G' => count('G')}
+
+    dna_nucleotides.each_with_object({}) do |nucleotide, result|
+      result[nucleotide] = count(nucleotide)
+    end
   end
 
 end
