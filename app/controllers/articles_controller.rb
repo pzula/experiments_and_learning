bc5c26873948d2a9f3a2b8c7db88cf5b99022aeb
Persa Zula <persa@persazula.com>
2013-11-01 18:38:22 -0600

    Adds create and delete functionality

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,5 +6,25 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
   end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    @article.save
+
+    redirect_to article_path(@article)
+  end
+
+  def destroy
+    @article = Article.find(params[:id]).destroy
+    redirect_to articles_path
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :body)
+  end
 end
 
