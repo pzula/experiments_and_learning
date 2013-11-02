9a7c58a8862f03137e3bdd147553e686833156b1
Persa Zula <persa@persazula.com>
2013-11-01 19:56:00 -0600

    I1 Complete: CRUD in place with flash messaging

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -15,16 +15,35 @@ class ArticlesController < ApplicationController
     @article = Article.new(article_params)
     @article.save
 
+    flash.notice = "Article '#{@article.title}' Created!"
+
     redirect_to article_path(@article)
   end
 
   def destroy
     @article = Article.find(params[:id]).destroy
+
+    flash.notice = "Article '#{@article.title}' Deleted!"
+
     redirect_to articles_path
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    @article.update(article_params)
+
+    flash.notice = "Article '#{@article.title}' Updated!"
+
+    redirect_to article_path(@article)
   end
 
   def article_params
     params.require(:article).permit(:title, :body)
   end
+
 end
 
