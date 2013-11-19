87c3d73e4e43d5a379d7d8c2d9cb2f102de0a413
Persa Zula <persa@persazula.com>
2013-11-19 15:09:31 -0700

    First iteration through hamming in JS

--- hamming.js
+++ hamming.js
@@ -1,8 +1,21 @@
 var Hamming = function() {
   this.compute = function(strand1, strand2) {
-    return 0;
-  }
 
+      if (strand1.length < strand2.length) {
+        var smallerStrand = strand1;
+      } else {
+        var smallerStrand = strand2;
+      }
+
+    diffcount = 0;
+
+    for(var i = 0; i < smallerStrand.length; i++) {
+      if (strand1[i] != strand2[i]) {
+        diffcount += 1;
+      }
+    }
+    return diffcount;
+  }
 }
 
 
