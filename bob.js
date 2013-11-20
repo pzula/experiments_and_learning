1edb714f983e46965dc3ea51fbfcec26972ff964
Persa Zula <persa@persazula.com>
2013-11-20 14:45:14 -0700

    Refactoring bob for readability

--- bob.js
+++ bob.js
@@ -1,19 +1,33 @@
 var Bob = function() {
 	this.hey = function(input) {
 
-		if (input.search(/[a-zA-Z]/) != -1 && input.toUpperCase() === input) {
+		if (this.isShouting(input)) {
 			return "Whoa, chill out!";
-		} else if (input[input.length - 1] === "?") {
+		} else if (this.isAQuestion(input)) {
 			return "Sure"
-		} else if (input.trim() === "") {
+		} else if (this.isSilence(input)) {
       return "Fine. Be that way!";
     } else {
 			return "Whatever.";
 		}
 	}
+
+	this.isShouting = function(input) {
+		return input.search(/[a-zA-Z]/) != -1 && input.toUpperCase() === input;
+	}
+
+	this.isAQuestion = function(input) {
+		return input[input.length - 1] === "?";
+	}
+
+	this.isSilence = function(input) {
+		return input.trim() === "";
+	}
 }
 
-// exports.Bob = Bob; // doesn't work in browsers
+// exports.Bob = Bob;
+// doesn't work in browsers
+
 
 
 // // class methods in JS
