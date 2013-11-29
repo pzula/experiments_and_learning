4d01128984e36aedac9849acb1c81d8373f10a62
Persa Zula <persa@persazula.com>
2013-11-29 15:55:25 -0500

    Validates email address

--- app/models/email_address.rb
+++ app/models/email_address.rb
@@ -1,3 +1,4 @@
 class EmailAddress < ActiveRecord::Base
   belongs_to :Person
+  validates :email, presence: true
 end
--- spec/models/email_address_spec.rb
+++ spec/models/email_address_spec.rb
@@ -1,4 +1,16 @@
 require 'spec_helper'
 
 describe EmailAddress do
+  let(:emailaddress) do
+    EmailAddress.new(email: 'example@example.com') 
+  end
+
+  it 'is valid' do
+    expect(emailaddress).to be_valid
+  end
+
+  it 'is invalid without an email address' do
+    emailaddress.email = nil
+    expect(emailaddress).to_not be_valid
+  end
 end
