522c137cf1ca0aee814a8360a22df00f2a4f73d8
Persa Zula <persa@persazula.com>
2013-11-29 16:07:30 -0500

    Updates email validations to require reference to a person

--- app/models/email_address.rb
+++ app/models/email_address.rb
@@ -1,4 +1,5 @@
 class EmailAddress < ActiveRecord::Base
   belongs_to :Person
   validates :email, presence: true
+  validates :person_id, presence: true
 end
--- spec/models/email_address_spec.rb
+++ spec/models/email_address_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe EmailAddress do
   let(:emailaddress) do
-    EmailAddress.new(email: 'example@example.com') 
+    EmailAddress.new(email: 'example@example.com', person_id: 1)
   end
 
   it 'is valid' do
@@ -11,6 +11,11 @@ describe EmailAddress do
 
   it 'is invalid without an email address' do
     emailaddress.email = nil
+    expect(emailaddress).to_not be_valid
+  end
+
+  it 'must have a reference to a person' do
+    emailaddress.person_id = nil
     expect(emailaddress).to_not be_valid
   end
 end
