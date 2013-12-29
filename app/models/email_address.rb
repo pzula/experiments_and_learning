941110480cb8ea9024c8265e085eced62ffcfe3a
Persa Zula <persa@persazula.com>
2013-12-29 15:01:29 -0500

    Updates email addresses to use Contact polymorphic relationship

--- app/models/email_address.rb
+++ app/models/email_address.rb
@@ -1,5 +1,6 @@
 class EmailAddress < ActiveRecord::Base
   belongs_to :person
   validates :email, presence: true
-  validates :person_id, presence: true
+  validates :contact_id, presence: true
+  belongs_to :contact, polymorphic: true
 end
--- spec/models/company_spec.rb
+++ spec/models/company_spec.rb
@@ -22,4 +22,10 @@ describe Company do
     phone_number = company.phone_numbers.build(number: "333-4444")
     expect(phone_number.number).to eq('333-4444')
   end
+
+  it "responds with its created email address" do
+    company.email_addresses.build(email: 'xyco@example.com')
+    expect(company.email_addresses.map(&:email)).to eq(['xyco@example.com'])
+  end
+
 end
