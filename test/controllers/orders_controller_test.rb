b28cfb710d98d970aa47c1852279aed77796a4bf
Persa Zula <persa@persazula.com>
2013-12-31 12:11:31 -0700

    Refactors to use API test helpers)

--- test/controllers/orders_controller_test.rb
+++ test/controllers/orders_controller_test.rb
@@ -4,37 +4,20 @@ class OrdersControllerTest < ActionController::TestCase
   include  APIControllerTest
 
   def test_it_shows_orders
-    # Setup any necessary data
     2.times{ Order.create }
-
-    # Make a JSON request
-    get :index, :format => :json
-
-    # Check that the request is ok
-    assert_response :success
-
-    # Check that we get the right data
-    data = JSON.parse(@response.body)
+    data = get_json(:index)
     assert_equal 2, data.count
   end
 
   def test_it_responds_with_a_single_order
     order = Order.create
-    get :show, :id => order.id, :format => :json
-
-    assert_response :success
-    data = JSON.parse(@response.body)
+    data = get_json(:show, :id => order.id)
     assert_equal order.id, data["id"]
   end
 
   def test_it_creates_an_order
-    #build the data parameters
-    #submit that to the create url
-    #check that we get back a 201 created
-
     params = {:order => {:amount => 9.00, :user_id => 2},  :format => :json}
     post :create, params
-    #assert_equal 201, @response.status
     assert_status 201
   end
 end
