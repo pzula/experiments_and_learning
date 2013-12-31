035a35cdc081facecc2f959e2c33a2906f696a54
Persa Zula <persa@persazula.com>
2013-12-31 11:19:25 -0700

    Adding API tests

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,9 +1,13 @@
 class OrdersController < ApplicationController
+  respond_to :json, :html
+
   def index
     @orders = Order.all
+    respond_with @orders
   end
 
   def show
     @order = Order.find(params[:id])
+    respond_with @order
   end
 end
--- test/controllers/orders_controller_test.rb
+++ test/controllers/orders_controller_test.rb
@@ -1,7 +1,23 @@
 require 'test_helper'
 
 class OrdersControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def test_it_shows_orders
+    # Setup any necessary data
+    2.times{ Order.create }
+
+    # Make a JSON request
+    get :index, :format => :json
+
+    # Check that the request is ok
+    assert_response :success
+
+    # Check that we get the right data
+    data = JSON.parse(@response.body)
+    assert_equal 2, data.count
+  end
+
+  def test_it_responds_with_a_single_order
+    order = Order.create
+    get :show, :id => order.id, :format => :json
+  end
 end
